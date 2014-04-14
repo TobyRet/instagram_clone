579d9506db8f85c7b19293e40f0c45723f0812f1
Mikhail Dubov <mdubov@gmail.com>
2014-04-14 12:03:35 +0100

    Installed Devise and generated User model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 InstagramClone::Application.routes.draw do
+
+  devise_for :users
+  root to: "posts#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
