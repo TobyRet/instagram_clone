0c2e291447f1cdff60e2666bf3fc7d1c1e91a1e5
Mikhail Dubov <mdubov@gmail.com>
2014-04-14 13:25:57 +0100

    Added useername

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,9 @@
 class PostsController < ApplicationController
 
-	def new
-	end
-	
+  def index
+  end
+
+  def new
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140414110219) do
+ActiveRecord::Schema.define(version: 20140414114311) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20140414110219) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
