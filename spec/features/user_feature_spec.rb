0e2bdd25a8b8bf8a38fcdced21778b02b7797796
Mario Gintili <mariojgintili@gmail.com>
2014-04-14 16:33:11 +0100

    eliminated unnecessary test

--- spec/features/user_feature_spec.rb
+++ spec/features/user_feature_spec.rb
@@ -2,12 +2,4 @@ require 'spec_helper'
 
 describe User do
 
-
-
-  context "Sign up" do
-    it "should be possible to sign up username" do
-      login_as_test_user
-      expect(page).to have_content "successfully"
-    end
-  end
 end
