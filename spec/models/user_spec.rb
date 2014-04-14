44ec1f9e06ad684d8de0c8fe85c578241026da27
Mikhail Dubov <mdubov@gmail.com>
2014-04-14 12:20:51 +0100

    Added test for username

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,22 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+
+
+  context "Sign up" do
+    it "should be possible to sign up username" do
+      visit '/users/sign_up'
+      fill_in 'Username', with 'test'
+      fill_in 'Email', with 'test@test.com'
+      fill_in 'Password', with '12345678'
+      fill_in 'Password confirmation', with '12345678'
+      click_button 'Sign up'
+      expect(page).to have_content 'Successful'
+
+    end
+
+    it "should return an error if an incorrect login is entered during sign in" do
+    end
+  end
 end
